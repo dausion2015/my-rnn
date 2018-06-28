8b68f1d01279f0071bf61a1be6ffb7a07a5b52f4
dausion <dausion@vip.qq.com>
2018-06-28 15:13:28 +0800

    23

--- model.py
+++ model.py
@@ -3,8 +3,8 @@
 
 import numpy as np
 import tensorflow as tf
-# from flags import parse_args
-# FLAGS,unparse = parse_args()
+from flags import parse_args
+FLAGS,unparse = parse_args()
 
 
 class Model():
